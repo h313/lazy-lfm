55434cd3bb4be0cbb1d450c7e538794d7b736ed6
Harry <arandomawesomegeek@gmail.com>
2017-05-23 17:28:12 -0400

    h5py output, showing stats about each track

---
+++ config.py
@@ -0,0 +1,24 @@
+import pylast
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+
+# You have to have your own unique two values for Last.fm keys
+# Obtain yours from http://www.last.fm/api/account/create for Last.fm
+lastfm_public_key = ""
+lastfm_secret_key = ""
+lastfm_username = ""
+lastfm_password = ""
+# Uncomment one of these to get stats on the top songs
+time_period = 'PERIOD_OVERALL'
+# time_period = 'PERIOD_12MONTHS'
+# time_period = 'PERIOD_6MONTHS'
+# time_period = 'PERIOD_3MONTHS'
+
+SPOTIPY_CLIENT_ID=''
+SPOTIPY_CLIENT_SECRET=''
+
+client_credentials_manager = SpotifyClientCredentials(client_id=SPOTIPY_CLIENT_ID, client_secret=SPOTIPY_CLIENT_SECRET)
+spotify = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+
+network = pylast.LastFMNetwork(api_key=lastfm_public_key, api_secret=lastfm_secret_key,
+                               username=lastfm_username, password_hash=pylast.md5(lastfm_password))
--- run.py
+++ run.py
@@ -1,10 +1,42 @@
-import spotipy
-import pylast
-import hashlib
-from config import network
+from tables import *
+from config import network, time_period, lastfm_username, spotify
 
-spotify = spotipy.Spotify()
 
-artist = network.get_artist("System of a Down")
+class Track(IsDescription):
+    song = StringCol(32)
+    album = StringCol(32)
+    danceability = Float32Col()
+    loudness = Float32Col()
+    acousticness = Float32Col()
+    instrumentalness = Float32Col()
+    liveness = Float32Col()
+    valence = Float32Col()
 
-print(artist)
+
+h5file = open_file("output.h5", mode="w", title="Spotify Tracks")
+group = h5file.create_group(h5file.root, 'trackinfo', 'track information')
+
+table = h5file.create_table(group, 'trackinfo', Track)
+track = table.row
+
+top = network.get_user(lastfm_username).get_top_tracks(time_period)
+
+for i in range(30):
+    result = spotify.search(q='track:"' + top[i].item.title + '"artist:"' + top[i].item.artist.name + '"',
+                             type='track')
+
+    analysis = spotify.audio_features(result['tracks']['items'][0]['uri'])[0]
+
+    track['song'] = result['tracks']['items'][0]['name']
+    track['album'] = result['tracks']['items'][0]['album']['name']
+    track['danceability'] = analysis['danceability']
+    track['loudness'] = analysis['loudness']
+    track['acousticness'] = analysis['acousticness']
+    track['instrumentalness'] = analysis['instrumentalness']
+    track['liveness'] = analysis['liveness']
+    track['valence'] = analysis['valence']
+    track.append()
+    table.flush()
+
+print(h5file)
+h5file.close()
